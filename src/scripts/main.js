2131500f3fbcf02e63cfa90f31f1e63f57c7835f
Sam Pita <samanthakpita@gmail.com>
2019-10-16 13:13:50 -0500

    finished mirror mirror exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 console.log("Life is beautiful")
+
+const inputField = document.querySelector(".textInput")
+
+const articleEl1 = document.getElementById("firstArticleEl")
+const articleEl2 = document.getElementById("secondArticleEl")
+
+
+let mirrorText = function (event) {
+    articleEl1.textContent = inputField.value.style.wordWrap
+    articleEl2.textContent = inputField.value.style.wordWrap
+}
+
+inputField.addEventListener("keyup", mirrorText)
+
+
+
+
